f469ca73de9e3da4251cef6e35d2b6d250b6929e
Amirhossein Dashtizade <amirhossein.d.z@gmail.com>
2019-05-07 19:25:12 +0430

    Refactor

--- stringtouls.php
+++ stringtouls.php
@@ -1,68 +1,114 @@
 <?php
 
-public class StringToul
+class StringTool
 {
-
-    public function concat($a, $b)
+    /**
+     *  Concatenate two strings (Alias for the "concatenate" method)
+     *
+     * @param string $a First string to concatenate
+     * @param string $b Second string to concatenate
+     * @return string
+     */
+    public static function concat(string $a, string $b)
     {
-        $full = $a . $b;
-        return $full
+        return self::concatenate($a, $b);
     }
 
-
-    private function writeLn($a)
+    /**
+     * Add line break to the end of the string
+     *
+     * @param string $a String to add line break at the end of it
+     * @return string
+     */
+    public static function writeLn(string $a)
     {
-        echo $a . "\n";
+        return $a . "\n";
     }
 
-    public static function UpperCase($string) {
-        $result = strtoupper($string);
+    /**
+     * Make a string uppercase
+     *
+     * @param string $string String to be uppercase
+     * @return string
+     */
+    public static function toUppercase(string $string) {
         return strtoupper($string);
     }
-    public function to_lower_case($input) {
+
+    /**
+     * Make a string lowercase
+     *
+     * @param string $input String to be lowercase
+     * @return string
+     */
+    public static function toLowercase(string $input) {
         return strtolower($input);
     }
 
-    /* calc hash */
-    public function hash($input) {
-        $hashed = md5();
-        return $hashed;
+    /**
+     * Calculate the hash value of the input string
+     *
+     * @param string $input String to be hashed
+     * @param string $algorithm Algorithm to use for hashing
+     * @return string
+     */
+    public static function hash(string $input, string $algorithm = 'md5') {
+        return hash($algorithm, $input);
     }
 
-    public function md5($input)
+    /**
+     * Calculate the md5 hash of the input string
+     *
+     * @param string $input String to calculate the md5 hash of it
+     * @return string
+     */
+    public static function md5(string $input)
     {
-        $hashed = md5($input);
-        return $hashed;
+        return md5($input);
     }
 
 
-    // Calc sha512 hash
-    public function sha512($input)   {
-        $hashed = sha($input);
-        return $hashed;
+    /**
+     *  Calculate sha512 hash of a string
+     *
+     * @param $input String to calculate the sha512 hash of it
+     * @return string
+     */
+    public static function sha512($input)   {
+        return static::hash($input, 'sha512');
     }
-	
-	
-	public function concatenate($a, $b, $c)
+
+    /**
+     *  Concatenate two strings
+     *
+     * @param string $a First string to concatenate
+     * @param string $b Second string to concatenate
+     * @return string
+     */
+	public static function concatenate($a, $b)
     {
-        $new = $a . $b;
-        return $new
+        return $a. $b;
     }
-
 
     /**
      * @param string $a Needle to search for
      * @param string $b Replace - value to replace the needle
      * @param string $c Haystack - string to search the needle in
+     * @return mixed
      */
-    public function replce($a, $b, $c)
+    public static function replace($a, $b, $c)
     {
-        retrun str_replace($a, $b, $c);
+        return str_replace($a, $b, $c);
     }
 
-
+    /**
+     * Stripe whitespaces from the beginning and end of a string
+     *
+     * @param $input String to be trimmed
+     * @return string
+     */
     public static function trim($input)
     {
-        $trimmed = trim($input);
+        return trim($input);
     }
 }
